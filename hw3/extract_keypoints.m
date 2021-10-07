6e984f1fc490aeec78e8af3b7f33bcf4d18c3ae0
sht99 <44593416+sht99@users.noreply.github.com>
2021-10-06 20:50:01 -0400

    further work, still incomplete

--- hw3/extract_keypoints.m
+++ hw3/extract_keypoints.m
@@ -2,15 +2,35 @@ function [x, y, scores, Ih, Iv] = extract_keypoints(image)
 %UNTITLED3 Summary of this function goes herw
 
 k = 0.05;
+x = [];
+y = [];
+scores = [];
 im = imread(image);
 im = rgb2gray(im);
-% imshow(im);
+im = im2double(im);
+
+%imshow(im);
 num_rows = size(im, 1);
 num_cols = size(im, 2);
 
-rsltMatrix = zeros([num_rows num_cols]);
+R = zeros([num_rows num_cols]);
 
+h1 = [-1 0 1];
+h2 = [-1 0 1]';
+Ih = imfilter(im, h1);
+Iv = imfilter(im, h2);
 
+Ih2 = Ih.^2;
+Iv2 = Iv.^2;
+Ixy = Ih.*Iv;
+
+for i = 1:num_rows
+    for j = 1:num_cols
+        M = [Ih2(i,j) Ixy(i,j);Ixy(i,j) Iv2(i,j)]; 
+        R(i,j) = det(M)-0.01*(trace(M))^2;
+        
+    end
+end
 
 
 end
--- hw3/show_image.m
+++ hw3/show_image.m
@@ -1,2 +1,9 @@
 image = 'cardinal1.jpg';
+im = imread(image);
 [x, y, scores, Ih, Iv] = extract_keypoints(image);
+%imshow(Ih);
+imshow(Iv);
+subplot(1,2,1);
+imshow(Ih);
+subplot(1,2,2);
+imshow(Iv);
