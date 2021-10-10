7f49be1ef2fe9ff7f0075a124a125125238fbb65
sht99 <44593416+sht99@users.noreply.github.com>
2021-10-10 09:19:45 -0400

    added hw3 that does the job

--- hw3/extract_keypoints.m
+++ hw3/extract_keypoints.m
@@ -1,22 +1,25 @@
 function [x, y, scores, Ih, Iv] = extract_keypoints(image)
-%UNTITLED3 Summary of this function goes herw
 
+%set up and preprocessing
 k = 0.05;
+window_size = 5;
+thres = 10000000;
 x = [];
 y = [];
 scores = [];
-im = imread(image);
-im = rgb2gray(im);
+
+im = rgb2gray(image);
 im = im2double(im);
 
-%imshow(im);
+
 num_rows = size(im, 1);
 num_cols = size(im, 2);
 
 R = zeros([num_rows num_cols]);
 
-h1 = [-1 0 1];
-h2 = [-1 0 1]';
+%Gradients
+h1 = [-1 0 1;-1 0 1;-1 0 1];
+h2 = [-1 0 1; -1 0 1; -1 0 1]';
 Ih = imfilter(im, h1);
 Iv = imfilter(im, h2);
 
@@ -24,11 +27,44 @@ Ih2 = Ih.^2;
 Iv2 = Iv.^2;
 Ixy = Ih.*Iv;
 
-for i = 1:num_rows
+
+%computing Matrix and R
+for i = 3:num_rows-3
+    for j = 3:num_cols-3
+        pixel = 1;
+        for n =1:window_size
+            for z =1:window_size
+                X(pixel) = Ih2(i-3+n,j-3+z);
+                Y(pixel) = Iv2(i-3+n, j-3+z);
+                XY(pixel) = Ixy(i-3+n, j-3+z);
+                pixel= pixel+1;
+            end
+        end
+        M = [sum(X), sum(XY); sum(XY),sum(Y)];
+        R(i,j) = det(M) - k*(trace(M)).^2;    
+    end
+end
+%threshold r and remove unwanted values
+Rvect = R(:);
+r_sort = sort(Rvect);
+[s,~] = size(Rvect);
+thres = r_sort(s-100);
+
+R(R < thres) = 0;
+imagesc(R);
+num_rows = size(R, 1);
+num_cols = size(R, 2);
+
+%create x and y and scores vectors
+for i=1: num_rows
     for j = 1:num_cols
-        M = [Ih2(i,j) Ixy(i,j);Ixy(i,j) Iv2(i,j)]; 
-        R(i,j) = det(M)-0.01*(trace(M))^2;
-        
+        ind = 1;
+        if R(i,j) > 0
+            x = [x,j];
+            y = [y,i];
+            scores = [scores,R(i,j)];
+            ind=ind+1;
+        end
     end
 end
 
--- hw3/show_image.m
+++ hw3/show_image.m
@@ -1,9 +1,22 @@
-image = 'cardinal1.jpg';
+clear 
+% have to change file name manually
+image = 'checkbox.jpg';
 im = imread(image);
-[x, y, scores, Ih, Iv] = extract_keypoints(image);
-%imshow(Ih);
-imshow(Iv);
-subplot(1,2,1);
-imshow(Ih);
-subplot(1,2,2);
-imshow(Iv);
+%extract needed variables to show corners on top of image
+[x, y, scores, Ih, Iv] = extract_keypoints(im);
+
+figure;
+
+%add circles at the corneres detected by algorithm.
+% have to modify  "...scores(i) / 50" per picture to better show circles
+imshow(im)
+hold on
+for i =1:101
+    for j =1:101
+        plot(x(i),y(i),'ro','MarkerSize',scores(i) / 50);
+        
+    end
+end
+        
+hold off
+
